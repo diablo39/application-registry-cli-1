d0fac71416f2a316b2c102ba1f1686b22e325d9a
Roman Glogowski <roman.glogowski@bankmillennium.pl>
2019-10-20 20:16:02 +0200

    adding http lcient for backend

---
+++ src/ApplicationRegistry.BackendHttpClient/ServerClient.cs
@@ -0,0 +1,26 @@
+﻿using ApplicationRegistry.Collector.Model;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationRegistry.BackendHttpClient
+{
+    public class ServerClient
+    {
+        private readonly HttpClient _client;
+        private readonly string _baseUrl;
+
+        public ServerClient(HttpClient client, string baseUrl)
+        {
+            _client = client;
+            _baseUrl = baseUrl;
+        }
+
+        public async Task ReportError (CollectorError error)
+        {
+            await _client.PostAsJsonAsync("/api/v1/Collector/Error", error);
+        }
+    }
+}
