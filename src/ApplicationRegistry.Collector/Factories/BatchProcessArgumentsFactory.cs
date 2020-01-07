90ba8ab95921f1227cf48237780a524713c2146a
Roman Glogowski <roman.glogowski@bankmillennium.pl>
2020-01-07 09:31:14 +0100

    tests

--- src/ApplicationRegistry.Collector/Factories/BatchProcessArgumentsFactory.cs
+++ src/ApplicationRegistry.Collector/Factories/BatchProcessArgumentsFactory.cs
@@ -5,106 +5,103 @@ using System.Linq;
 
 namespace ApplicationRegistry.Collector
 {
-    partial class Program
+    internal class BatchProcessArgumentsFactory
     {
-        class BatchProcessArgumentsFactory
+        public BatchProcessArguments Create(string application, string environment, string fileOutput, string projectFilePath, string solutionFilePath, Uri url, string version)
         {
-            public BatchProcessArguments Create(string application, string environment, string fileOutput, string projectFilePath, string solutionFilePath, Uri url, string version)
+            var arguments = new BatchProcessArguments
             {
-                var arguments = new BatchProcessArguments
-                {
-                    Application = application,
-                    Environment = environment,
-                    FileOutput = fileOutput,
-                    ProjectFilePath = projectFilePath,
-                    SolutionFilePath = solutionFilePath,
-                    Url = url,
-                    Version = version
-                };
+                Application = application,
+                Environment = environment,
+                FileOutput = fileOutput,
+                ProjectFilePath = projectFilePath,
+                SolutionFilePath = solutionFilePath,
+                Url = url,
+                Version = version
+            };
 
-                NewMethod(arguments);
-                return arguments;
+            NewMethod(arguments);
+            return arguments;
+        }
+
+        private void NewMethod(BatchProcessArguments arguments)
+        {
+            arguments.ProjectFilePath = arguments.ProjectFilePath.TrimEnd('"');
+            arguments.ProjectFilePath = System.IO.Path.GetFullPath(arguments.ProjectFilePath);
+
+            if (!arguments.ProjectFilePath.EndsWith(".csproj", StringComparison.InvariantCultureIgnoreCase)) // directory provided
+            {
+                var files = Directory.EnumerateFiles(arguments.ProjectFilePath, "*.csproj").ToList();
+
+                if (files.Count == 0)
+                {
+                    "Project file not found. Please try to set full path to your *.csproj file".LogCritical(this);
+                    return;
+                }
+
+                if (files.Count > 1)
+                {
+                    Console.Error.WriteLine("Error: More than one project found in directory {0}", arguments.ProjectFilePath); // TODO: try to use result as store
+                    Console.Error.WriteLine("Set full path to the project file");
+
+                    return; //throw
+                }
+
+                arguments.ProjectFilePath = files[0];
             }
 
-            private void NewMethod(BatchProcessArguments arguments)
+            if (string.IsNullOrWhiteSpace(arguments.SolutionFilePath))
             {
-                arguments.ProjectFilePath = arguments.ProjectFilePath.TrimEnd('"');
-                arguments.ProjectFilePath = System.IO.Path.GetFullPath(arguments.ProjectFilePath);
+                var success = FindSolutionFile(arguments);
 
-                if (!arguments.ProjectFilePath.EndsWith(".csproj", StringComparison.InvariantCultureIgnoreCase)) // directory provided
+                if (!success)
                 {
-                    var files = Directory.EnumerateFiles(arguments.ProjectFilePath, "*.csproj").ToList();
-
-                    if (files.Count == 0)
-                    {
-                        "Project file not found. Please try to set full path to your *.csproj file".LogCritical(this);
-                        return;
-                    }
-
-                    if (files.Count > 1)
-                    {
-                        Console.Error.WriteLine("Error: More than one project found in directory {0}", arguments.ProjectFilePath); // TODO: try to use result as store
-                        Console.Error.WriteLine("Set full path to the project file");
-
-                        return; //throw
-                    }
-
-                    arguments.ProjectFilePath = files[0];
-                }
-
-                if (string.IsNullOrWhiteSpace(arguments.SolutionFilePath))
-                {
-                    var success = FindSolutionFile(arguments);
-
-                    if (!success)
-                    {
-                        Console.Error.WriteLine("Solution file can't be found, try to set seolution file location explicitly"); // TODO: try to use result as store
-                        return; //throw
-                    }
-                }
-                else
-                {
-                    arguments.SolutionFilePath = Path.GetFullPath(arguments.SolutionFilePath);
+                    Console.Error.WriteLine("Solution file can't be found, try to set seolution file location explicitly"); // TODO: try to use result as store
+                    return; //throw
                 }
             }
-
-            private static bool FindSolutionFile(BatchProcessArguments arguments)
+            else
             {
-                bool success = false;
-
-                var directory = new FileInfo(arguments.ProjectFilePath).Directory;
-                do
-                {
-                    var files = directory.GetFiles("*.sln");
-
-                    if (files.Length == 1)
-                    {
-                        arguments.SolutionFilePath = files[0].FullName;
-                        success = true;
-                        break;
-                    }
-
-                    if (files.Length > 1)
-                    {
-                        Console.Error.WriteLine("More than one solution file found.");
-                        Console.Error.WriteLine("Found:");
-                        for (int i = 0; i < files.Length; i++)
-                        {
-                            Console.Error.WriteLine($"* {files[i].FullName}");
-                        }
-                        Console.Error.WriteLine("Use parametr --solution to specify correct solution file");
-                        success = false;
-                        break;
-                    }
-
-
-                    directory = directory.Parent;
-                }
-                while (directory != null);
-                return success;
+                arguments.SolutionFilePath = Path.GetFullPath(arguments.SolutionFilePath);
             }
         }
 
+        private static bool FindSolutionFile(BatchProcessArguments arguments)
+        {
+            bool success = false;
+
+            var directory = new FileInfo(arguments.ProjectFilePath).Directory;
+            do
+            {
+                var files = directory.GetFiles("*.sln");
+
+                if (files.Length == 1)
+                {
+                    arguments.SolutionFilePath = files[0].FullName;
+                    success = true;
+                    break;
+                }
+
+                if (files.Length > 1)
+                {
+                    Console.Error.WriteLine("More than one solution file found.");
+                    Console.Error.WriteLine("Found:");
+                    for (int i = 0; i < files.Length; i++)
+                    {
+                        Console.Error.WriteLine($"* {files[i].FullName}");
+                    }
+                    Console.Error.WriteLine("Use parametr --solution to specify correct solution file");
+                    success = false;
+                    break;
+                }
+
+
+                directory = directory.Parent;
+            }
+            while (directory != null);
+            return success;
+        }
     }
 
 }
+
---
+++ tests/ApplicationRegistry.Collector.Tests/Factories/BatchProcessArgumentsFactoryTests.cs
@@ -0,0 +1,71 @@
+﻿using ApplicationRegistry.Collector.Tests.TestingInfrastructure;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ApplicationRegistry.Collector.Tests.Factories
+{
+    public class BatchProcessArgumentsFactoryTests : IClassFixture<LoggingContext>
+    {
+        public BatchProcessArgumentsFactoryTests(LoggingContext context) { }
+        
+        [Theory]
+        [MemberData(nameof(DataForHappyPath))]
+        public void Factory_Schould_Sanitaze_Path_With_Correct_Result(string projectFilePath, string solutionFilePath, string expectedProjectFile, string expectedSolutionFile)
+        {
+            var factory = new BatchProcessArgumentsFactory();
+            var arguments = factory.Create("application", "env", "", projectFilePath, solutionFilePath, null, "");
+
+            var currentProjectFile = arguments.ProjectFilePath;
+            var currentSolutionFile = arguments.SolutionFilePath;
+
+            currentProjectFile.Should().Be(expectedProjectFile);
+            currentSolutionFile.Should().Be(expectedSolutionFile);
+        }
+
+
+        //[Fact]
+        //public void Factory_Nonexisting_ProjectFile_Path_ShouldFail()
+        //{
+        //    var batch = new SanitazeApplicationArgumentsBatch();
+        //    var context = new BatchContext(new BatchProcessArguments
+        //    {
+        //        ProjectFilePath = Path.GetFullPath(".")
+        //    });
+
+        //    var result = await batch.ProcessAsync(context);
+
+        //    var currentProjectFile = context.Arguments.ProjectFilePath;
+        //    var currentSolutionFile = context.Arguments.SolutionFilePath;
+
+
+        //    result.Should().NotBeNull();
+        //    result.Result.Should().Be(BatchExecutionResult.ExecutionResult.Fail);
+        //    //currentProjectFile.Should().Be(expectedProjectFile);
+        //    //currentSolutionFile.Should().Be(expectedSolutionFile);
+        //}
+
+        public static IEnumerable<object[]> DataForHappyPath
+        {
+            get
+            {
+                var projectFileRelativePath = "../../../ApplicationRegistry.Collector.Tests.csproj";
+                var projectDirectoryRelativePath = "../../../";
+                var expectedProjectFile = Path.GetFullPath("../../../ApplicationRegistry.Collector.Tests.csproj");
+                var solutionFilePathRelative = "../../../../../ApplicationRegistry.Collector.sln";
+                var expectedSolutionFilePath = Path.GetFullPath("../../../../../ApplicationRegistry.Collector.sln");
+
+                return new List<object[]>
+                    {
+                        new object[] { projectFileRelativePath, null, expectedProjectFile, expectedSolutionFilePath },
+                        new object[] { projectDirectoryRelativePath, null, expectedProjectFile, expectedSolutionFilePath },
+                        new object[] { projectFileRelativePath, solutionFilePathRelative, expectedProjectFile, expectedSolutionFilePath },
+                    };
+            }
+        }
+    }
+}
