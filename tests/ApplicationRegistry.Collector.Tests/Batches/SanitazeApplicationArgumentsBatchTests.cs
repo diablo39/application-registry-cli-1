819e01eeb9d328a7801e14f7bc5206325d603896
Roman Glogowski <18666546+diablo39@users.noreply.github.com>
2020-01-18 13:16:45 +0100

    manage parameters

--- tests/ApplicationRegistry.Collector.Tests/Batches/SanitazeApplicationArgumentsBatchTests.cs
+++ tests/ApplicationRegistry.Collector.Tests/Batches/SanitazeApplicationArgumentsBatchTests.cs
@@ -1,84 +1,85 @@
-﻿//using ApplicationRegistry.Collector.Batches;
-//using System;
-//using System.Collections.Generic;
-//using System.Text;
-//using System.Threading.Tasks;
-//using Xunit;
-//using FluentAssertions;
-//using System.IO;
-//using System.Reflection;
-//using ApplicationRegistry.Collector.Tests.TestingInfrastructure;
+﻿using ApplicationRegistry.Collector.Batches;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using FluentAssertions;
+using System.IO;
+using System.Reflection;
+using ApplicationRegistry.Collector.Tests.TestingInfrastructure;
 
-//namespace ApplicationRegistry.Collector.Tests.Batches
-//{
-//    public class SanitazeApplicationArgumentsBatchTests: IClassFixture<LoggingContext>
-//    {
-//        public SanitazeApplicationArgumentsBatchTests(LoggingContext context)
-//        {
+namespace ApplicationRegistry.Collector.Tests.Batches
+{
+    public class SanitazeApplicationArgumentsBatchTests : IClassFixture<LoggingContext>
+    {
+        public SanitazeApplicationArgumentsBatchTests(LoggingContext context)
+        {
 
-//        }
+        }
 
-//        [Theory]
-//        [MemberData(nameof(DataForHappyPath))]
-//        public async Task Batch_Schould_Sanitaze_Path_With_Correct_Result(string projectFilePath, string solutionFilePath, string expectedProjectFile, string expectedSolutionFile)
-//        {
-//            var batch = new SanitazeApplicationArgumentsBatch();
-//            var context = new BatchContext(new BatchProcessArguments {
-//                ProjectFilePath = projectFilePath,
-//                SolutionFilePath = solutionFilePath
-//            });
-            
-//            var result = await batch.ProcessAsync(context);
+        [Theory]
+        [MemberData(nameof(DataForHappyPath))]
+        public async Task Batch_Schould_Sanitaze_Path_With_Correct_Result(string projectFilePath, string solutionFilePath, string expectedProjectFile, string expectedSolutionFile)
+        {
+            var batch = new SanitazeApplicationArgumentsBatch();
+            var context = new BatchContext(new BatchProcessArguments
+            {
+                ProjectFilePath = projectFilePath,
+                SolutionFilePath = solutionFilePath
+            });
 
-//            var currentProjectFile = context.Arguments.ProjectFilePath;
-//            var currentSolutionFile = context.Arguments.SolutionFilePath;
+            var result = await batch.ProcessAsync(context);
 
-
-//            result.Should().NotBeNull();
-//            result.Result.Should().Be(BatchExecutionResult.ExecutionResult.Success);
-//            currentProjectFile.Should().Be(expectedProjectFile);
-//            currentSolutionFile.Should().Be(expectedSolutionFile);
-//        }
+            var currentProjectFile = context.Arguments.ProjectFilePath;
+            var currentSolutionFile = context.Arguments.SolutionFilePath;
 
 
-//        [Fact]
-//        public async Task Batch_Nonexisting_ProjectFile_Path_ShouldFail()
-//        {
-//            var batch = new SanitazeApplicationArgumentsBatch();
-//            var context = new BatchContext(new BatchProcessArguments
-//            {
-//                ProjectFilePath = Path.GetFullPath(".")
-//            });
-
-//            var result = await batch.ProcessAsync(context);
-
-//            var currentProjectFile = context.Arguments.ProjectFilePath;
-//            var currentSolutionFile = context.Arguments.SolutionFilePath;
+            result.Should().NotBeNull();
+            result.Result.Should().Be(BatchExecutionResult.ExecutionResult.Success);
+            currentProjectFile.Should().Be(expectedProjectFile);
+            currentSolutionFile.Should().Be(expectedSolutionFile);
+        }
 
 
-//            result.Should().NotBeNull();
-//            result.Result.Should().Be(BatchExecutionResult.ExecutionResult.Fail);
-//            //currentProjectFile.Should().Be(expectedProjectFile);
-//            //currentSolutionFile.Should().Be(expectedSolutionFile);
-//        }
-        
-//        public static IEnumerable<object[]> DataForHappyPath
-//        {
-//            get
-//            {
-//                var projectFileRelativePath = "../../../ApplicationRegistry.Collector.Tests.csproj";
-//                var projectDirectoryRelativePath = "../../../";
-//                var expectedProjectFile = Path.GetFullPath("../../../ApplicationRegistry.Collector.Tests.csproj");
-//                var solutionFilePathRelative = "../../../../../ApplicationRegistry.Collector.sln";
-//                var expectedSolutionFilePath = Path.GetFullPath("../../../../../ApplicationRegistry.Collector.sln");
+        [Fact]
+        public async Task Batch_Nonexisting_ProjectFile_Path_ShouldFail()
+        {
+            var batch = new SanitazeApplicationArgumentsBatch();
+            var context = new BatchContext(new BatchProcessArguments
+            {
+                ProjectFilePath = Path.GetFullPath(".")
+            });
 
-//                return new List<object[]>
-//                {
-//                    new object[] { projectFileRelativePath, null, expectedProjectFile, expectedSolutionFilePath },
-//                    new object[] { projectDirectoryRelativePath, null, expectedProjectFile, expectedSolutionFilePath },
-//                    new object[] { projectFileRelativePath, solutionFilePathRelative, expectedProjectFile, expectedSolutionFilePath },
-//                };
-//            }
-//        }
-//    }
-//}
+            var result = await batch.ProcessAsync(context);
+
+            var currentProjectFile = context.Arguments.ProjectFilePath;
+            var currentSolutionFile = context.Arguments.SolutionFilePath;
+
+
+            result.Should().NotBeNull();
+            result.Result.Should().Be(BatchExecutionResult.ExecutionResult.Fail);
+            //currentProjectFile.Should().Be(expectedProjectFile);
+            //currentSolutionFile.Should().Be(expectedSolutionFile);
+        }
+
+        public static IEnumerable<object[]> DataForHappyPath
+        {
+            get
+            {
+                var projectFileRelativePath = "../../../ApplicationRegistry.Collector.Tests.csproj";
+                var projectDirectoryRelativePath = "../../../";
+                var expectedProjectFile = Path.GetFullPath("../../../ApplicationRegistry.Collector.Tests.csproj");
+                var solutionFilePathRelative = "../../../../../ApplicationRegistry.Collector.sln";
+                var expectedSolutionFilePath = Path.GetFullPath("../../../../../ApplicationRegistry.Collector.sln");
+
+                return new List<object[]>
+                {
+                    new object[] { projectFileRelativePath, null, expectedProjectFile, expectedSolutionFilePath },
+                    new object[] { projectDirectoryRelativePath, null, expectedProjectFile, expectedSolutionFilePath },
+                    new object[] { projectFileRelativePath, solutionFilePathRelative, expectedProjectFile, expectedSolutionFilePath },
+                };
+            }
+        }
+    }
+}
